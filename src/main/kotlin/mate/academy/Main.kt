f81430cea8136c3dbc8ff50746cf880b01683f23
Vitaliy Tishik <vitisik54@gmail.com>
2026-04-01 22:41:03 +0300

    ref

--- src/main/kotlin/mate/academy/Main.kt
+++ src/main/kotlin/mate/academy/Main.kt
@@ -1,14 +1,14 @@
 package mate.academy
 
-fun greetUsers() {
-    while (true) {
-        print("Please enter a name (or press Enter to quit): \n")
-        val name = readLine() ?: ""
-        if (name.isEmpty()) break
-        println("Hello, $name!")
-    }
-}
-
 fun main() {
+    fun greetUsers() {
+        while (true) {
+            print("Please enter a name (or press Enter to quit): ")
+            val name = readLine() ?: ""
+            if (name.isEmpty()) break
+            println("Hello, $name!")
+        }
+    }
+
     greetUsers()
 }
